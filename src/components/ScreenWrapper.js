2626a366849f03ac096fa519fc53bd88218c8c39
codeprakhar25 <prakharkhatri123@gmail.com>
2023-07-04 13:56:17 +0530

    Folder Structure

---
+++ src/components/ScreenWrapper.js
@@ -0,0 +1,21 @@
+import { Platform, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ScreenWrapper = ({children}) => {
+  return (
+    <View style={styles.screenWrapper}>
+      {children}
+    </View>
+  )
+}
+
+export default ScreenWrapper
+
+const styles = StyleSheet.create({
+    screenWrapper:{
+        paddingTop:Platform.OS === 'ios' ? 60 : 0,
+        paddingHorizontal:24,
+        paddingBottom:32,
+        minHeight:'100%',
+    }
+})
---
+++ src/screens/AddNotes.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+const AddNotes = () => {
+  return (
+    <Text>AddNotes</Text>
+  )
+}
+
+export default AddNotes
